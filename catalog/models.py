4caf023ee4b8e090d9be0c471bb7bc52a56315fd
Nisrina <nisrina.fatimah@ui.ac.id>
2025-10-22 14:07:45 +0700

    update cart

--- catalog/models.py
+++ catalog/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    prodcut_name = models.CharField(max_length=255)
+    product_name = models.CharField(max_length=255)  
     stock = models.PositiveIntegerField(default=1)
     thumbnail = models.URLField(blank=True, null=True)
     inStock = models.BooleanField(default=True)
-    description = models.TextField
-    price = models.PositiveIntegerField
-    
-    def __str__(self):
-        return f"{self.product_name} {self.price}"
+    description = models.TextField()  
+    price = models.PositiveIntegerField() 
+
+    def _str_(self):
+        return f"{self.product_name} - Rp{self.price:,}"
