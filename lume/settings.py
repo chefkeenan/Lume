35679caae57d2ae2296511b33a99b1a9e0400740
Cytiez <qilla.abrar@gmail.com>
2025-10-22 16:19:19 +0700

    perbaikin main

--- lume/settings.py
+++ lume/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'checkout',
     'user',
     'user_admin',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -66,7 +67,7 @@ ROOT_URLCONF = 'lume.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
