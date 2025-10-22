bae7e62e7d2d9fdf319efbab3aee4f6dcf9297d8
Jumaannjii <135129916+Jumaannjii@users.noreply.github.com>
2025-10-23 02:33:45 +0700

    tambah baru

--- bookingkelas/admin.py
+++ bookingkelas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+# bookingkelas/admin.py
+from django.contrib import admin
+from .models import ClassSessions
+
+@admin.register(ClassSessions)
+class ClassSessionsAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "instructor", "room", "time", "price", "capacity_max")
+    list_filter = ("category", "instructor", "room")
+    search_fields = ("title", "instructor", "description")
