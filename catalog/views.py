03cbf5969f1b9377e891fa87a2536614133e047d
Cytiez <qilla.abrar@gmail.com>
2025-10-22 14:09:57 +0700

    kerjaan views yang bakal ditimpa

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from catalog.forms import ProductForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseForbidden, JsonResponse, HttpResponseRedirect
 from django.core import serializers
@@ -20,3 +21,18 @@ def show_json(request):
     products_list = Product.objects.all()
     data = [product_to_dict(products_list)]
     return JsonResponse(data, safe = False)
+
+def add_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == 'POST':
+        product_entry = form.save(commit = False)
+        product_entry.user = request.user
+        product_entry.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "add_product.html", context)
