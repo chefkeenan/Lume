73a911d0926f76d1e5525727470c562523ccfda5
Jumaannjii <135129916+Jumaannjii@users.noreply.github.com>
2025-10-24 23:23:50 +0700

    update dikit

--- lume/settings.py
+++ lume/settings.py
@@ -149,7 +149,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static' # merujuk ke /static root project pada mode development
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode production
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
