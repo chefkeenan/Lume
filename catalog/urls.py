f789e9e2590396a1252b8457c4e8709df0e2f9a5
Cytiez <qilla.abrar@gmail.com>
2025-10-26 04:21:24 +0700

    benerin

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("products/", views.product_list, name="product_list"),
     path("products/add/", views.product_create, name="product_add"),
     path("products/add-modal/", views.product_add_modal, name="product_add_modal"),
-    path("products/create-modal/", views.product_create_modal, name="product_create_modal"),
+    path("products/create-modal/", views.product_create, name="product_create_modal"),
 
     # pakai uuid
     path("products/<uuid:pk>/edit-modal/", views.product_edit_modal, name="product_edit_modal"),
--- catalog/views.py
+++ catalog/views.py
@@ -66,15 +66,29 @@ def product_delete(request, pk):
 
 @login_required
 @user_passes_test(is_admin)
-@require_http_methods(["POST", "GET"])
+@require_http_methods(["POST"])
 def product_create(request):
     form = ProductForm(request.POST or None, request.FILES or None)
-    if request.method == "POST" and form.is_valid():
+
+    is_ajax = request.headers.get("x-requested-with") == "XMLHttpRequest"
+    if form.is_valid():
         obj = form.save()
-        if request.headers.get("x-requested-with") == "XMLHttpRequest":
-            card_html = render_to_string("product_card.html", {"p": obj}, request=request)
+        if is_ajax:
+            card_html = render_to_string("product_card.html", {"p": obj, "user": request.user}, request=request)
             return JsonResponse({"ok": True, "card_html": card_html, "id": str(obj.pk)})
+        # fallback non-AJAX
         return redirect("main:show_main")
+
+    # INVALID:
+    if is_ajax:
+        form_html = render_to_string(
+            "catalog/_product_form.html",
+            {"form": form, "obj": None},
+            request=request,
+        )
+        return JsonResponse({"ok": False, "form_html": form_html}, status=200)
+
+    # fallback non-AJAX
     return render(request, "catalog/add_product.html", {"form": form})
 
 
