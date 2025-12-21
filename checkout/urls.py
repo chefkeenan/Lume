c826b1be2d0d501ce6e9402be541052ceab5e79f
Keenan <aryasatyakeenan@gmail.com>
2025-12-21 22:46:38 +0700

    tes

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,9 +4,7 @@ from . import views, api
 app_name = "checkout"
 
 urlpatterns = [
-    # ======================================
-    # HTML Views (untuk website)
-    # ======================================
+
     path("cart/", views.cart_checkout_page, name="cart_checkout_page"),
     path("cart/create/", views.checkout_cart_create, name="checkout_cart_create"),
     path("cart/summary/", views.cart_summary_json, name="cart_summary_json"),
@@ -15,9 +13,7 @@ urlpatterns = [
 
     path("confirmed/", views.order_confirmed, name="order_confirmed"),
 
-    # ======================================
-    # API Endpoints (untuk Flutter)
-    # ======================================
+
     path("api/cart-summary/", api.cart_summary_api, name="cart_summary_api"),
     path("api/cart-checkout/", api.cart_checkout_api, name="cart_checkout_api"),
     path("api/booking-checkout/<int:booking_id>/", api.booking_checkout_api,name="booking_checkout_api"),
